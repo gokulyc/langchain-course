a7d6c03ed31f57d5ba574c286cf186bc978db87e
Gokul Y <gokulyc@gmail.com>
2025-10-18 21:33:19 +0530

    updated model to openai/gpt-5

--- llms.py
+++ llms.py
@@ -3,7 +3,7 @@ from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(
     temperature=0,
-    model="openai/gpt-5-mini",
+    model="openai/gpt-5",
     api_key=os.getenv("OPENROUTER_API_KEY"),
     base_url="https://openrouter.ai/api/v1",
 )
